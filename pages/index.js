a28291e95658a0e4eba0a4bf6a153b842fe39e04
IsaqueAlvess <ysak.alves@outlook.com>
2025-02-18 18:54:16 -0300

    formatando linguagem.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Brenda eu te amo, sua linda maravilhosa. Me dê um beijão por favore ❤️ </h1>
+    return <h1>Brenda eu te amo, sua linda maravilhosa.Se gostou sorria e me dê um beijão ❤️ </h1>
 }
 
 export default Home;
